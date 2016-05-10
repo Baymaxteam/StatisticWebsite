b2e76a4d5bf8fbb0467b09bc00da4b1652e38c6c
Alex <qwedsazxc78@gmail.com>
2016-05-10 21:23:18 +0800

    assign js folder

---
+++ static/js/chartP5/histogramfit.js
@@ -0,0 +1,26 @@
+function HistogramFitchart() {
+    $('#P12chartHistogramFit .chart').highcharts({
+        title: {
+            text: Chart1Title
+        },
+        xAxis: {
+            categories: Chart1CategoryX
+        },
+        labels: {
+            items: [{
+                html: Chart1Title,
+                style: {
+                    left: '50px',
+                    top: '18px',
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'black'
+                }
+            }]
+        },
+        yAxis: {
+            title: {
+                text: Chart1TitleY
+            },
+        },
+        series: Chart1Col
+    });
+};
