02dcb6f2eff35ecc92e6a239999d809385e47a82
Alex <qwedsazxc78@gmail.com>
2016-05-20 20:30:39 +0800

    map click event

--- static/js/chartPM25/TWmap.js
+++ static/js/chartPM25/TWmap.js
@@ -1,106 +1,35 @@
-$(function () {
+$(function() {
 
     // Prepare demo data
     var data = [
-        // {
-        //     "hc-key": "tw-pt",
-        //     "value": 0
-        // },
-        // {
-        //     "hc-key": "tw-tn",
-        //     "value": 1
-        // },
-        // {
-        //     "hc-key": "tw-il",
-        //     "value": 2
-        // },
-        // {
-        //     "hc-key": "tw-ch",
-        //     "value": 3
-        // },
-        // {
-        //     "hc-key": "tw-tt",
-        //     "value": 4
-        // },
-        // {
-        //     "hc-key": "tw-ph",
-        //     "value": 5
-        // },
-        // {
-        //     "hc-key": "tw-km",
-        //     "value": 6
-        // },
-        // {
-        //     "hc-key": "tw-lk",
-        //     "value": 7
-        // },
-        // {
-        //     "hc-key": "tw-tw",
-        //     "value": 8
-        // },
-        // {
-        //     "hc-key": "tw-cs",
-        //     "value": 9
-        // },
+
         {
             "hc-key": "tw-th",
             "value": 10
         },
-        // {
-        //     "hc-key": "tw-yl",
-        //     "value": 11
-        // },
-        // {
-        //     "hc-key": "tw-kh",
-        //     "value": 12
-        // },
-        // {
-        //     "hc-key": "tw-tp",
-        //     "value": 0
-        // },
-        // {
-        //     "hc-key": "tw-hs",
-        //     "value": 14
-        // },
-        // {
-        //     "hc-key": "tw-hh",
-        //     "value": 15
-        // },
-        // {
-        //     "hc-key": "tw-cl",
-        //     "value": 16
-        // },
-        // {
-        //     "hc-key": "tw-ml",
-        //     "value": 17
-        // },
-        // {
-        //     "hc-key": "tw-ty",
-        //     "value": 18
-        // },
+
         {
             "hc-key": "tw-cg",
             "value": 19
         },
-        // {
-        //     "hc-key": "tw-hl",
-        //     "value": 20
-        // },
+
         {
             "hc-key": "tw-nt",
             "value": 21
         }
     ];
 
+    var selectCityName = "Taichung City"; // default city = Taichung City
+
     // Initiate the chart
     $('#container').highcharts('Map', {
 
-        title : {
-            text : 'PM2.5 氣體分布資訊'
+        title: {
+            text: 'PM2.5 氣體分布資訊'
         },
 
-        subtitle : {
-            text : 'Source map: <a href="https://code.highcharts.com/mapdata/countries/tw/tw-all.js">Taiwan</a>'
+        subtitle: {
+            text: 'Source map: <a href="https://code.highcharts.com/mapdata/countries/tw/tw-all.js">Taiwan</a>'
         },
 
         mapNavigation: {
@@ -113,12 +42,27 @@ $(function () {
         colorAxis: {
             min: 0
         },
+        plotOptions: {
+            series: {
+                point: {
+                    events: {
+                        click: function() {
+                            // alert(this.name);
+                            selectCityName = this.name;
+                            console.log(selectCityName);
 
-        series : [{
-            data : data,
+                        }
+
+                    }
+                }
+            }
+        },
+
+        series: [{
+            data: data,
             mapData: Highcharts.maps['countries/tw/tw-all'],
             joinBy: 'hc-key',
-            name: 'Random data',
+            name: 'PM25',
             states: {
                 hover: {
                     color: '#BADA55'
@@ -131,3 +75,14 @@ $(function () {
         }]
     });
 });
+
+
+// if (cityName == "Taichung City") {
+//     selectCityName = 
+// } else if (cityName == "Nantou") {
+
+// } else if (cityName == "Changhua") {
+
+// } else {
+
+// }
