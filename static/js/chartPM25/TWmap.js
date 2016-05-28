463a3449c9a4ef5c194f5f5f7a29e93f56569d7a
Alex <qwedsazxc78@gmail.com>
2016-05-29 01:09:43 +0800

    get PM25 data

--- static/js/chartPM25/TWmap.js
+++ static/js/chartPM25/TWmap.js
@@ -11,103 +11,22 @@ var sparkDataSet3 = [" 0, 0, 13, 13, 14 ; column", " 14, 14, 13, 13, 14 ; column
 ];
 
 var putSparkdata = sparkDataSet1;
-
 var DEBUG_Log = true;
 
 $(function() {
 
     // Prepare demo data
-    var data = [{
-            "hc-key": "tw-th",
-            "value": 10
-        },
-        {
-            "hc-key": "tw-cg",
-            "value": 19
-        },
-        {
-            "hc-key": "tw-nt",
-            "value": 21
-        }
-    ];
-
     $.get('/ajax_selectFilePart2/', {
         'fileName': 'PM2.5_data_2015.csv'
     }, function(respons) {
         DEBUG("Server response the json data : ");
         DEBUG(respons);
     });
-    var selectCityName = "Taichung City"; // default city = Taichung City
 
-    // Initiate the chart
-    $('#container').highcharts('Map', {
 
-        title: {
-            text: 'PM2.5 氣體分布資訊'
-        },
-
-        subtitle: {
-            text: 'Source map: <a href="https://code.highcharts.com/mapdata/countries/tw/tw-all.js">Taiwan</a>'
-        },
-
-        mapNavigation: {
-            enabled: true,
-            buttonOptions: {
-                verticalAlign: 'bottom'
-            }
-        },
-
-        colorAxis: {
-            min: 0
-        },
-        plotOptions: {
-            series: {
-                point: {
-                    events: {
-                        click: function() {
-                            // alert(this.name);
-                            selectCityName = this.name;
-                            console.log(selectCityName);
-                            if (selectCityName === "Nantou") {
-                                selectCityName = "埔里";
-                                putSparkdata = sparkDataSet1;
-                            } else if (selectCityName === "Changhua") {
-                                selectCityName = "線西";
-                                putSparkdata = sparkDataSet2;
-                            } else {
-                                selectCityName = "忠明";
-                                putSparkdata = sparkDataSet3;
-
-                            }
-                            doChunk();
-
-                            $('#selectCityName').text("本年度資訊 : " + selectCityName);
-                        }
-
-                    }
-                }
-            }
-        },
-
-        series: [{
-            data: data,
-            mapData: Highcharts.maps['countries/tw/tw-all'],
-            joinBy: 'hc-key',
-            name: 'PM25',
-            states: {
-                hover: {
-                    color: '#BADA55'
-                }
-            },
-            dataLabels: {
-                enabled: true,
-                format: '{point.name}'
-            }
-        }]
-    });
+    plotTWMapChart($('#containerTWMap'));
 
     // interactive with sparklines
-
     Highcharts.SparkLine = function(a, b, c) {
         var hasRenderToArg = typeof a === 'string' || a.nodeName,
             options = arguments[hasRenderToArg ? 1 : 0],
@@ -207,77 +126,147 @@ $(function() {
             new Highcharts.Chart(options, b);
     };
 
-    var start = +new Date(),
-        $tds = $('td[data-sparkline]'),
-        fullLen = $tds.length,
-        n = 0;
-
+    doChunk(sparkDataSet1);
 
     // Creating 153 sparkline charts is quite fast in modern browsers, but IE8 and mobile
     // can take some seconds, so we split the input into chunks and apply them in timeouts
     // in order avoid locking up the browser process and allow interaction.
-    function doChunk() {
-        var time = +new Date(),
-            i,
-            len = $tds.length,
-            $td,
-            stringdata,
-            arr,
-            data,
-            chart;
-        console.log(len);
-        for (i = 0; i < len; i += 1) {
-            $td = $($tds[i]);
-            stringdata = putSparkdata[i];
-            arr = stringdata.split('; ');
-            data = $.map(arr[0].split(', '), parseFloat);
-            chart = {};
 
-            if (arr[1]) {
-                chart.type = arr[1];
-            }
-            $td.highcharts('SparkLine', {
-                series: [{
-                    data: data,
-                    pointStart: 1,
-                    color: Highcharts.getOptions().colors[i + 5],
-                }],
-                tooltip: {
-                    headerFormat: '<span style="font-size: 10px">' + $td.parent().find('th').html() + ', point{point.x}:</span><br/>',
-                    pointFormat: '<b>{point.y}.0</b>'
-                },
-                exporting: {
-                    enabled: false
-                },
-                chart: chart
-            });
-            // console.log(stringdata);
-            // console.log(arr);
-            // console.log(data);
-
-
-            n += 1;
-
-            // If the process takes too much time, run a timeout to allow interaction with the browser
-            if (new Date() - time > 500) {
-                $tds.splice(0, i + 1);
-                setTimeout(doChunk, 0);
-                break;
-            }
-
-            // Print a feedback on the performance
-            // if (n === fullLen) {
-            //     $('#result').html('Generated ' + fullLen + ' sparklines in ' + (new Date() - start) + ' ms');
-            // }
-        }
-    }
-
-    doChunk();
 
 });
 
 function DEBUG(printData) {
     if (DEBUG_Log === true) {
         console.log(printData)
+    }
+}
+
+function plotTWMapChart(DOM) {
+    var data = [{
+        "hc-key": "tw-th",
+        // "value": 10
+    }, {
+        "hc-key": "tw-cg",
+        // "value": 19
+    }, {
+        "hc-key": "tw-nt",
+        // "value": 21
+    }];
+
+    DOM.highcharts('Map', {
+        title: {
+            text: 'PM2.5 氣體分布資訊'
+        },
+        mapNavigation: {
+            enabled: true,
+            buttonOptions: {
+                verticalAlign: 'bottom'
+            }
+        },
+
+        colorAxis: {
+            min: 0
+        },
+        plotOptions: {
+            series: {
+                point: {
+                    events: {
+                        click: function() {
+                            // alert(this.name);
+                            var selectCityName = this.name;
+                            console.log(selectCityName);
+                            if (selectCityName === "Nantou") {
+                                doChunk(sparkDataSet1);
+                            } else if (selectCityName === "Changhua") {
+                                doChunk(sparkDataSet2);
+                            } else {
+                                doChunk(sparkDataSet3);
+                            }
+                            $('#selectCityName').text("本年度資訊 : " + selectCityName);
+                        }
+
+                    }
+                }
+            }
+        },
+
+        series: [{
+            data: data,
+            mapData: Highcharts.maps['countries/tw/tw-all'],
+            joinBy: 'hc-key',
+            name: 'PM25',
+            states: {
+                hover: {
+                    color: '#BADA55'
+                }
+            },
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }]
+    });
+
+}
+
+function doChunk(selectCityData) {
+    var start = +new Date(),
+        $tds = $('td[data-sparkline]'),
+        fullLen = $tds.length,
+        n = 0;
+    var time = +new Date(),
+        i,
+        len = $tds.length,
+        $td,
+        stringdata,
+        arr,
+        data,
+        chart;
+
+
+    for (i = 0; i < len; i += 1) {
+
+        $td = $($tds[i]);
+        stringdata = selectCityData[i];
+        arr = stringdata.split('; ');
+        data = $.map(arr[0].split(', '), parseFloat);
+        chart = {};
+
+        if (arr[1]) {
+            chart.type = arr[1];
+        }
+        $td.highcharts('SparkLine', {
+            series: [{
+                data: data,
+                pointStart: 1,
+                color: Highcharts.getOptions().colors[i + 5],
+            }],
+            tooltip: {
+                headerFormat: '<span style="font-size: 10px">' + $td.parent().find('th').html() + ', point{point.x}:</span><br/>',
+                pointFormat: '<b>{point.y}.0</b>'
+            },
+            exporting: {
+                enabled: false
+            },
+            chart: chart
+        });
+        // console.log(stringdata);
+        // console.log(arr);
+        // console.log(data);
+
+
+        n += 1;
+
+        // If the process takes too much time, run a timeout to allow interaction with the browser
+        if (new Date() - time > 500) {
+            $tds.splice(0, i + 1);
+            setTimeout(doChunk, 0);
+            break;
+        }
+
+        // Print a feedback on the performance
+        // if (n === fullLen) {
+        //     $('#result').html('Generated ' + fullLen + ' sparklines in ' + (new Date() - start) + ' ms');
+        // }
     }
 }
