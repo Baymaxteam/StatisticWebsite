269f092179387052fece0cf7d5ea093385f1eb1c
Alex <qwedsazxc78@gmail.com>
2016-06-12 01:57:18 +0800

    change to colume

--- static/js/chartp5/emergency.js
+++ static/js/chartp5/emergency.js
@@ -1,14 +1,12 @@
-$(function () {
+$(function() {
 
     var updateFlag = false;
     var DEBUG_Log = false;
     var addPointIndex = 50;
-    
 
-    function DEBUG(printData) 
-    {
-        if (DEBUG_Log === true) 
-        {
+
+    function DEBUG(printData) {
+        if (DEBUG_Log === true) {
             console.log(printData)
         }
     }
@@ -26,26 +24,25 @@ $(function () {
         var sensorData = respons.data;
         var sensorStatList = respons.statList;
         var data = [];
-        data1  = [],
-        data2  = [],
-        data3  = [],
-        data4  = [],
-        data5  = [],
-        timeData = 
-        [
-            1104510000000,
-            1136040000000,
-            1167580000000,
-            1199120000000,
-            1230650000000,
-            1262190000000,
-            1293720000000,
-            1325260000000,
-            1356800000000,
-            1388330000000,
-            1419870000000 
+        data1 = [],
+            data2 = [],
+            data3 = [],
+            data4 = [],
+            data5 = [],
+            timeData = [
+                1104510000000,
+                1136040000000,
+                1167580000000,
+                1199120000000,
+                1230650000000,
+                1262190000000,
+                1293720000000,
+                1325260000000,
+                1356800000000,
+                1388330000000,
+                1419870000000
 
-        ];
+            ];
 
         var SelectTableHeader = [];
         SelectTableHeader.push({ title: '統計量' });
@@ -59,17 +56,17 @@ $(function () {
         };
         // 整理sensor data
         for (i = 0; i < len; i++) {
-            data1.push([timeData[i],sensorData[0][i]]);
-            data2.push([timeData[i],sensorData[1][i]]);
-            data3.push([timeData[i],sensorData[2][i]]);
-            data4.push([timeData[i],sensorData[3][i]]);
-            data5.push([timeData[i],sensorData[4][i]]);
+            data1.push([timeData[i], sensorData[0][i]]);
+            data2.push([timeData[i], sensorData[1][i]]);
+            data3.push([timeData[i], sensorData[2][i]]);
+            data4.push([timeData[i], sensorData[3][i]]);
+            data5.push([timeData[i], sensorData[4][i]]);
         };
-        data.push(data1,data2,data3,data4,data5);
+        data.push(data1, data2, data3, data4, data5);
 
         emergency($('#container7'), data);
 
-        
+
         //統計圖表
         $('#tableEm table').DataTable({
             data: sensorStatList,
@@ -99,57 +96,57 @@ $(function () {
 });
 
 
-function emergency(DOM, data)
-{
-        // Create the chart
-        DOM.highcharts('StockChart', {
-            exporting: { 
-                enabled: false 
-            },
+function emergency(DOM, data) {
+    // Create the chart
+    DOM.highcharts('StockChart', {
+        exporting: {
+            enabled: false
+        },
+        chart: {
+            type: 'column'
+        },
+        rangeSelector: {
+            selected: 5
+        },
 
-            rangeSelector : {
-                selected : 5
-            },
+        title: {
+            text: '急診數量'
+        },
 
-            title : {
-                text : '急診數量'
-            },
+        scrollbar: {
+            enabled: false
+        },
 
-            scrollbar : {
-                enabled : false
-            },
-
-            series : 
-            [{
-                name : '台北市',
-                data : data[0],
-                tooltip: {
-                    valueDecimals: 0
-                }
-            },{
-                name: '台中市',
-                data: data[1],
-                tooltip: {
-                    valueDecimals: 0
-                }
-            },{
-                name: '高雄市',
-                data: data[2],
-                tooltip: {
-                    valueDecimals: 0
-                }
-            },{
-                name: '新北市',
-                data: data[3],
-                tooltip: {
-                    valueDecimals: 0
-                }
-            },{
-                name: '花蓮市',
-                data: data[4],
-                tooltip: {
-                    valueDecimals: 0
-                }
-            }]
-        });
+        series: [{
+            name: '台北市',
+            data: data[0],
+            tooltip: {
+                valueDecimals: 0
+            }
+        }, {
+            name: '台中市',
+            data: data[1],
+            tooltip: {
+                valueDecimals: 0
+            }
+        }, {
+            name: '高雄市',
+            data: data[2],
+            tooltip: {
+                valueDecimals: 0
+            }
+        }, {
+            name: '新北市',
+            data: data[3],
+            tooltip: {
+                valueDecimals: 0
+            }
+        }, {
+            name: '花蓮市',
+            data: data[4],
+            tooltip: {
+                valueDecimals: 0
+            }
+        }]
+    });
 }
