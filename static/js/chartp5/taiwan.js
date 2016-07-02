3439f83de05f8703a20da1b5752cdb74cfde2cef
MomoHsieh <stevenshy1989@gmail.com>
2016-07-02 22:25:39 +0800

    20160702 2225

--- static/js/chartp5/taiwan.js
+++ static/js/chartp5/taiwan.js
@@ -93,7 +93,7 @@ $(function () {
             SelectTableHeader1.push({ title: titleList[i] });
         }
         // 整理sensor data
-        for (i = 0; i < len; i++) {
+        for (i = 1; i < len; i++) {
             heavy_l_data1.push(sensorData[i][0]);
             heavy_l_data2.push(sensorData[i][1]);
             heavy_l_data3.push(sensorData[i][2]);
@@ -122,7 +122,8 @@ $(function () {
         DEBUG(heavy_l_data);
 
         var k = 0;
-        taiwan($('#container'), heavy_l_array);
+        taiwan($('#container'), heavy_l_array, len);
+
         setInterval(function () 
         {
 
@@ -164,95 +165,6 @@ $(function () {
 
 function data_shift(loop, sensorData, len)
 {
-    var time_data = 
-    [
-        1264953540000,
-        1267372740000,
-        1270051140000,
-        1272643140000,
-        1275321540000,
-        1277913540000,
-        1280591940000,
-        1283270340000,
-        1285862340000,
-        1288540740000,
-        1291132740000,
-        1293811140000,
-        1296489540000,
-        1298908740000,
-        1301587140000,
-        1304179140000,
-        1306857540000,
-        1309449540000,
-        1312127940000,
-        1314806340000,
-        1317398340000,
-        1320076740000,
-        1322668740000,
-        1325347140000,
-        1328025540000,
-        1330531140000,
-        1333209540000,
-        1335801540000,
-        1338479940000,
-        1341071940000,
-        1343750340000,
-        1346428740000,
-        1349020740000,
-        1351699140000,
-        1354291140000,
-        1356969540000,
-        1359647940000,
-        1362067140000,
-        1364745540000,
-        1367337540000,
-        1370015940000,
-        1372607940000,
-        1375286340000,
-        1377964740000,
-        1380556740000,
-        1383235140000,
-        1385827140000,
-        1388505540000,
-        1391183940000,
-        1393603140000,
-        1396281540000,
-        1398873540000,
-        1401551940000,
-        1404143940000,
-        1406822340000,
-        1409500740000,
-        1412092740000,
-        1414771140000,
-        1417363140000,
-        1420041540000,
-        1422719940000,
-        1425139140000,
-        1427817540000,
-        1430409540000,
-        1433087940000,
-        1435679940000,
-        1438358340000,
-        1441036740000,
-        1443628740000,
-        1446307140000,
-        1448899140000,
-        1451577540000,
-        1454255940000,
-        1456761540000,
-        1459439940000,
-        1462031940000,
-        1464710340000,
-        1467302340000,
-        1469980740000,
-        1472659140000,
-        1475251140000,
-        1477929540000,
-        1480521540000,
-        1483199940000,
-
-    ];
-
 
     tc_data = [];
     tc_data1 = [],
@@ -278,28 +190,28 @@ function data_shift(loop, sensorData, len)
     tc_data21 = [],
     tc_data22 = [];
     for (i = loop; i < loop+12; i++) {
-                tc_data1.push([time_data[i],sensorData[i][0]]);
-                tc_data2.push([time_data[i],sensorData[i][1]]);
-                tc_data3.push([time_data[i],sensorData[i][2]]);
-                tc_data4.push([time_data[i],sensorData[i][3]]);
-                tc_data5.push([time_data[i],sensorData[i][4]]);
-                tc_data6.push([time_data[i],sensorData[i][5]]);
-                tc_data7.push([time_data[i],sensorData[i][6]]);
-                tc_data8.push([time_data[i],sensorData[i][7]]);
-                tc_data9.push([time_data[i],sensorData[i][8]]);
-                tc_data10.push([time_data[i],sensorData[i][9]]);
-                tc_data11.push([time_data[i],sensorData[i][10]]);
-                tc_data12.push([time_data[i],sensorData[i][11]]);
-                tc_data13.push([time_data[i],sensorData[i][12]]);
-                tc_data14.push([time_data[i],sensorData[i][13]]);
-                tc_data15.push([time_data[i],sensorData[i][14]]);
-                tc_data16.push([time_data[i],sensorData[i][15]]);
-                tc_data17.push([time_data[i],sensorData[i][16]]);
-                tc_data18.push([time_data[i],sensorData[i][17]]);
-                tc_data19.push([time_data[i],sensorData[i][18]]);
-                tc_data20.push([time_data[i],sensorData[i][19]]);
-                tc_data21.push([time_data[i],sensorData[i][20]]);
-                tc_data22.push([time_data[i],sensorData[i][21]]);
+                tc_data1.push([sensorData[i][0],sensorData[i][1]]);
+                tc_data2.push([sensorData[i][0],sensorData[i][2]]);
+                tc_data3.push([sensorData[i][0],sensorData[i][3]]);
+                tc_data4.push([sensorData[i][0],sensorData[i][4]]);
+                tc_data5.push([sensorData[i][0],sensorData[i][5]]);
+                tc_data6.push([sensorData[i][0],sensorData[i][6]]);
+                tc_data7.push([sensorData[i][0],sensorData[i][7]]);
+                tc_data8.push([sensorData[i][0],sensorData[i][8]]);
+                tc_data9.push([sensorData[i][0],sensorData[i][9]]);
+                tc_data10.push([sensorData[i][0],sensorData[i][10]]);
+                tc_data11.push([sensorData[i][0],sensorData[i][11]]);
+                tc_data12.push([sensorData[i][0],sensorData[i][12]]);
+                tc_data13.push([sensorData[i][0],sensorData[i][13]]);
+                tc_data14.push([sensorData[i][0],sensorData[i][14]]);
+                tc_data15.push([sensorData[i][0],sensorData[i][15]]);
+                tc_data16.push([sensorData[i][0],sensorData[i][16]]);
+                tc_data17.push([sensorData[i][0],sensorData[i][17]]);
+                tc_data18.push([sensorData[i][0],sensorData[i][18]]);
+                tc_data19.push([sensorData[i][0],sensorData[i][19]]);
+                tc_data20.push([sensorData[i][0],sensorData[i][20]]);
+                tc_data21.push([sensorData[i][0],sensorData[i][21]]);
+                tc_data22.push([sensorData[i][0],sensorData[i][22]]);
 
             };
     tc_data.push(tc_data1,tc_data2,tc_data3,tc_data4,tc_data5,tc_data6,tc_data7,tc_data8,tc_data9,tc_data10,tc_data11,tc_data12,tc_data13,tc_data14,tc_data15,tc_data16,tc_data17,tc_data18,tc_data19,tc_data20,tc_data21,tc_data22);
